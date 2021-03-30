a39cc125d61051774b09dcfbcf45c62b701cf60d
Alejandro Cayssials <admin@webxander.com>
2021-03-30 16:30:36 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,91 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <v-app>
+        <v-navigation-drawer dark v-model="drawer" app permanent>
+            <v-toolbar>
+                <v-app-bar-nav-icon
+                    @click="$root.$emit('drawer')"
+                ></v-app-bar-nav-icon>
+                <v-toolbar-title class="mx-3">
+                    <div class="logo">
+                        <h3>ClockXander</h3>
+                    </div>
+                </v-toolbar-title>
+            </v-toolbar>
+
+            <v-list dense rounded>
+                <v-list-item
+                    v-for="item in items"
+                    :key="item.title"
+                    :href="item.route"
+                >
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar app dark>
+            <v-spacer></v-spacer>
+
+            <v-btn
+                href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                target="_blank"
+                text
+            >
+                <span class="mr-2">Latest Release</span>
+                <v-icon>mdi-open-in-new</v-icon>
+            </v-btn>
+        </v-app-bar>
+
+        <v-main>
+            <HelloWorld />
+        </v-main>
+    </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import HelloWorld from './components/HelloWorld';
 
-#nav {
-  padding: 30px;
-}
+export default {
+    name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    components: {
+        HelloWorld
+    },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+    data: () => ({
+        drawer: true,
+        items: [
+            {
+                title: 'Resume',
+                icon: 'mdi-view-dashboard',
+                route: '/dashboard'
+            },
+            {
+                title: 'TodoList',
+                icon: 'mdi-format-list-checks',
+                route: '/todolist'
+            },
+            {
+                title: 'Clocks',
+                icon: 'mdi-clock-time-four-outline',
+                route: '/clocks'
+            },
+            {
+                title: 'Alarms',
+                icon: 'mdi-alarm-multiple',
+                route: '/alarms'
+            },
+            {
+                title: 'Time Tracker',
+                icon: 'mdi-timetable',
+                route: '/trackers'
+            }
+        ]
+    })
+};
+</script>
